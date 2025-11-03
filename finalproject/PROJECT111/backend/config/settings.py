4166bf9314f8eccbdb6737da5c5ffcdf233717c9
jinhi-112 <seojin072023@gmail.com>
2025-11-03 21:27:16 +0900

    Add STATIC_ROOT and disable collectstatic

--- finalproject/PROJECT111/backend/config/settings.py
+++ finalproject/PROJECT111/backend/config/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Add MEDIA_URL and MEDIA_ROOT for user-uploaded files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
